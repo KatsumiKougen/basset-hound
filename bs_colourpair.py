778b2d9eb30971865c517fbc1b38fc2b1a06053d
Katsumi Kougen <lesanonyme1001@gmail.com>
2024-01-29 23:11:05 +0700

    Add bs_colourpair.py

---
+++ bs_colourpair.py
@@ -0,0 +1,82 @@
+ColourPair = {
+    "black": {
+        "black": (0, 0),
+        "red": (0, 9),
+        "green": (0, 10),
+        "yellow": (0, 11),
+        "blue": (0, 12),
+        "magenta": (0, 13),
+        "cyan": (0, 14),
+        "white": (0, 15)
+    },
+    "red": {
+        "black": (9, 0),
+        "red": (9, 9),
+        "green": (9, 10),
+        "yellow": (9, 11),
+        "blue": (9, 12),
+        "magenta": (9, 13),
+        "cyan": (9, 14),
+        "white": (9, 15)
+    },
+    "green": {
+        "black": (10, 0),
+        "red": (10, 9),
+        "green": (10, 10),
+        "yellow": (10, 11),
+        "blue": (10, 12),
+        "magenta": (10, 13),
+        "cyan": (10, 14),
+        "white": (10, 15)
+    },
+    "yellow": {
+        "black": (11, 0),
+        "red": (11, 9),
+        "green": (11, 10),
+        "yellow": (11, 11),
+        "blue": (11, 12),
+        "magenta": (11, 13),
+        "cyan": (11, 14),
+        "white": (11, 15)
+    },
+    "blue": {
+        "black": (12, 0),
+        "red": (12, 9),
+        "green": (12, 10),
+        "yellow": (12, 11),
+        "blue": (12, 12),
+        "magenta": (12, 13),
+        "cyan": (12, 14),
+        "white": (12, 15)
+    },
+    "magenta": {
+        "black": (13, 0),
+        "red": (13, 9),
+        "green": (13, 10),
+        "yellow": (13, 11),
+        "blue": (13, 12),
+        "magenta": (13, 13),
+        "cyan": (13, 14),
+        "white": (13, 15)
+    },
+    "cyan": {
+        "black": (14, 0),
+        "red": (14, 9),
+        "green": (14, 10),
+        "yellow": (14, 11),
+        "blue": (14, 12),
+        "magenta": (14, 13),
+        "cyan": (14, 14),
+        "white": (14, 15)
+    },
+    "white": {
+        "black": (15, 0),
+        "red": (15, 9),
+        "green": (15, 10),
+        "yellow": (15, 11),
+        "blue": (15, 12),
+        "magenta": (15, 13),
+        "cyan": (15, 14),
+        "white": (15, 15)
+    },
+}
