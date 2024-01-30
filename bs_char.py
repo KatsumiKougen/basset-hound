eb0cb440b4fcd9013c5776f80b2c9d2148835d41
Katsumi Kougen <lesanonyme1001@gmail.com>
2024-01-30 21:53:09 +0700

    Add "Block" dictionary for Unicode Box Drawing characters

--- bs_char.py
+++ bs_char.py
@@ -3,3 +3,21 @@ Shade = {
     "MediumShade": "\u2592",
     "DarkShade": "\u2593"
 }
+
+Block = {
+    "UpperHalf": "\u2580",
+    "LowerHalf": "\u2584",
+    "LeftHalf": "\u258c",
+    "RightHalf": "\u2590",
+    "FullBlock": "\u2588",
+    "Quad3": "\u2596",
+    "Quad4": "\u2597",
+    "Quad1": "\u2598",
+    "Quad134": "\u2599",
+    "Quad14": "\u259a",
+    "Quad123": "\u259b",
+    "Quad124": "\u259c",
+    "Quad2": "\u259d",
+    "Quad23": "\u259e",
+    "Quad234": "\u259f"
+}
