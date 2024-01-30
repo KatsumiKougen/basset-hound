5b6e29c2be48e50d051a605d48f25917c997ebd8
Katsumi Kougen <lesanonyme1001@gmail.com>
2024-01-30 17:36:50 +0700

    Make changes to main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import bs_screen, bs_colourpair
 import curses
 
 AppScreen = bs_screen.Screen(curses.initscr())
-bs_screen.InitScreen(AppScreen)
-AppScreen.Start()
+AppScreen.InitScreen()
 AppScreen.UseColour(bs_colourpair.ColourPair)
-bs_screen.TerminateScreen(AppScreen)
+AppScreen.TerminateScreen()
