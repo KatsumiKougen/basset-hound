d96cbce1dd00e26a8eb62c893bf2c8e7490b2c16
Katsumi Kougen <lesanonyme1001@gmail.com>
2024-01-30 22:03:58 +0700

    Add "BoxDrawing" dictionary for Unicode Box Drawing characters

--- bs_char.py
+++ bs_char.py
@@ -10,6 +10,7 @@ Block = {
     "LeftHalf": "\u258c",
     "RightHalf": "\u2590",
     "FullBlock": "\u2588",
+    
     "Quad3": "\u2596",
     "Quad4": "\u2597",
     "Quad1": "\u2598",
@@ -27,8 +28,31 @@ Arrow = {
     "Up": "\u2191",
     "Right": "\u2192",
     "Down": "\u2193",
+
     "NorthWest": "\u2196",
     "NorthEast": "\u2197",
     "SouthWest": "\u2199",
     "SouthEast": "\u2198"
+}
+
+BoxDrawing = {
+    "Horizontal": "\u2500",
+    "Vertical": "\u2502",
+    "HorizontalDash": "\u2504",
+    "VerticalDash": "\u2506",
+
+    "DownLeft": "\u2510",
+    "DownRight": "\u250c",
+    "UpLeft": "\u2518",
+    "UpRight": "\u2514",
+    "VertLeft": "\u2524",
+    "VertRight": "\u251c",
+    "HoriUp": "\u2534",
+    "HoriDown": "\u252c",
+    "HoriVert": "\u253c",
+
+    "ArcDownLeft": "\u256e",
+    "ArcDownRight": "\u256d",
+    "ArcUpLeft": "\u256f",
+    "ArcUpRight": "\u2570",
 }
