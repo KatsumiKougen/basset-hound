7e72f62f92dc786143766357f66799047672f503
Katsumi Kougen <lesanonyme1001@gmail.com>
2024-02-04 17:18:12 +0700

    Reorganise the main code

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-import bs_screen, bs_colourpair
-import curses
+import bs_screen, bs_colourpair, bs_char
+import curses, time
 
-AppScreen = bs_screen.Screen(curses.initscr())
-AppScreen.InitScreen()
-AppScreen.UseColour(bs_colourpair.ColourPair)
-AppScreen.TerminateScreen()
+class ApplicationObject:
+
+    def __init__(self):
+        self.AppWindow = bs_screen.Screen(curses.initscr())
+        self.AppWindow.InitScreen()
+        self.AppWindow.UseColour(bs_colourpair.ColourPair)
+
+        self.DrawIntro()
+        self.AppWindow.Refresh()
+        self.AppWindow.GetChar()
+
+        self.AppWindow.TerminateScreen()
+
+    def DrawIntro(self):
+        ColumnLength = 24
+        for row in range(32):
+            for cidx, colour in enumerate((1, 3, 2, 4)):
+                for idx, char in enumerate(
+                    [bs_char.Braille["Dots123456"], *list(bs_char.Shade.values())]
+                ):
+                    self.AppWindow.PlotRect(
+                        row, ColumnLength//4*idx+ColumnLength*cidx,
+                        row, ColumnLength//4*(idx+1)-1+ColumnLength*cidx,
+                        char,
+                        self.AppWindow.ColourPair(0, colour)
+                    )
+
+App = ApplicationObject()
