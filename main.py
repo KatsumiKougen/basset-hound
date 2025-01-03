d5efb3f1d2487f4a037ab25ca364c3c93e64da84
Katsumi <lesanonyme1001@gmail.com>
2025-01-03 22:52:16 +0700

    Update code

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import bs_colourpair, bs_char
 from textual.app import *
 from textual.widgets import *
+from textual.containers import *
 
-projectName = "Untitled"
+BS_projectName = "Untitled"
+BS_canvasSize = [80, 25]
+BS_canvasCoordinate = [0, 0]
+
+class BS_Canvas(Static):
+    
+    def on_mount(self):
+        self.styles.width = BS_canvasSize[0]
+        self.styles.height = BS_canvasSize[1]
+
+class BS_CanvasContainer(Vertical):
+    
+    def compose(self):
+        yield BS_Canvas()
+        yield Label(f"{BS_canvasSize[0]}x{BS_canvasSize[1]}, coord: ({BS_canvasCoordinate[0]}, {BS_canvasCoordinate[1]})")
 
 class BS_AppObject(App):
     
@@ -12,11 +27,7 @@ class BS_AppObject(App):
     
     def compose(self):
         yield Header()
-        yield Static("basset-hound")
-        yield Button("exit", id="BS_ExitApp")
-    
-    def on_button_pressed(self, event: Button):
-        self.exit(event)
+        yield BS_CanvasContainer()
 
 if __name__ == "__main__":
     app = BS_AppObject()
