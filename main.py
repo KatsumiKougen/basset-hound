818da2139eebc113bc2b0b887686f32e330c3269
Katsumi <lesanonyme1001@gmail.com>
2025-01-03 19:03:28 +0700

    Update code

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import bs_screen, bs_colourpair, bs_char
-import textual
+from textual.app import *
+from textual.widgets import *
 
-class AppObject(textual.App):
+class BS_AppObject(App):
     
-    pass
+    TITLE = "basset-hound"
+    SUB_TITLE = "Untitled"
+    CSS_PATH = "bs_style.css"
+    
+    def compose(self):
+        print("composing")
+        yield Header()
+        yield Static("basset-hound")
+        yield Button("exit", id="BS_ExitApp")
+    
+    def on_button_pressed(self, event: Button):
+        print("pressed")
+        self.exit(event.id)
 
 if __name__ == "__main__":
-    app = AppObject()
+    app = BS_AppObject()
     app.run()
